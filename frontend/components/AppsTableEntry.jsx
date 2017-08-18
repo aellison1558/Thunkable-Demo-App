27a91125b7c2b2a8537402d48a84ed69fba3bc5b
Andrew Ellison <aellison1558@gmail.com>
2017-08-17 17:27:41 -0700

    add date to app display

--- frontend/components/AppsTableEntry.jsx
+++ frontend/components/AppsTableEntry.jsx
@@ -4,7 +4,10 @@ class AppsTableEntry extends React.Component {
 	render() {
 		return(
 			<div>
+				
 				{this.props.app.name}
+				\n
+				{this.props.app.lastEdited.toString()}
 			</div>
 		)
 	}
